c4f4288679c3cee47a39453ac8516e7024142856
tyler <tylerweitzman@gmail.com>
2025-04-14 04:42:29 -0700

    Save

--- src/Controllers/SecurityViewController.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <LocalAuthentication/LocalAuthentication.h>
-
-@interface SCISecurityViewController : UIViewController
-@end
--- src/Tweak.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-// * Tweak version *
-extern NSString *SCIVersionString;
-
-// Variables that work across features
-extern BOOL seenButtonEnabled; // Whether dm message seen button is enabled
-extern BOOL dmVisualMsgsViewedButtonEnabled; // Whether story dm unlimited views button is enabled
